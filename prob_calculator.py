b48f50e305eb6b6872098dba567ad4dfacff51ad
Shubbham Jain <shubham.jain368@gmail.com>
2022-03-19 17:34:27 +0530

    commit: probability calculator

---
+++ prob_calculator.py
@@ -0,0 +1,13 @@
+import copy
+import random
+
+# Consider using the modules imported above.
+
+
+class Hat:
+    def __init__(self):
+        print("Start")
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    print(hat, expected_balls, num_balls_drawn, num_experiments)
